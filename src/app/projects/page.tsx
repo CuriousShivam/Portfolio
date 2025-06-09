58d608de5b2e5fa3ab43cb9b5b8dd0d9ccd25039
CuriousShivam <shivam.sah2023t@gmail.com>
2025-06-09 20:40:11 +0530

    Minor Fixes 3

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,9 @@
-import MyComponent from "./ClientComponent";
 
 export default function Main() {
 
         return (
-            <div>
-                <MyComponent/>
-            </div>)
+            <div className={' p-3 bg-white border border-black text-center text-gray-500 animate-pulse'}>Comming Soon...</div>
+            )
 
 
 }
